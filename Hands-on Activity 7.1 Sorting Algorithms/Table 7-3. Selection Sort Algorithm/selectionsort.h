a803592acc706420c735c8dfb10d560f0a78cb3c
Andrei Santos <qarsantos02@tip.edu.ph>
2024-10-16 15:08:11 +0800

    Create selectionsort.h

---
+++ Hands-on Activity 7.1 Sorting Algorithms/Table 7-3. Selection Sort Algorithm/selectionsort.h
@@ -0,0 +1,10 @@
+#ifndef SELECTIONSORT_H
+#define SELECTIONSORT_H
+
+template <typename T>
+int Small_elmnt(T A[], int K, const int arrSize);
+
+template <typename T>
+void selc_sort(T arr[], const int N);
+
+#endif // SELECTIONSORT_H

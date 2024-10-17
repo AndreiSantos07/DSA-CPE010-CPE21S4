a6c5e3c55206e8b74437493ae07863847a038ce8
Andrei Santos <qarsantos02@tip.edu.ph>
2024-10-17 22:28:27 +0800

    Create Bubb_sr.h

---
+++ Hands-on Activity 7.1 Sorting Algorithms/Supplementary Activity/Bubb_sr.h
@@ -0,0 +1,18 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+void bubbleSort(int arr[], int size) {
+    // Bubble sort algorithm
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+#endif // BUBBLESORT_H

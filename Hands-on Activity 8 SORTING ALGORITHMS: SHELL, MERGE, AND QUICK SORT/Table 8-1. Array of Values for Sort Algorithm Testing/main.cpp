ad26b460ffb2d4911b2269f982be4535f8f18b88
Andrei Santos <qarsantos02@tip.edu.ph>
2024-10-21 15:17:29 +0800

    Update main.cpp

--- Hands-on Activity 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Table 8-1. Array of Values for Sort Algorithm Testing/main.cpp
+++ Hands-on Activity 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Table 8-1. Array of Values for Sort Algorithm Testing/main.cpp
@@ -9,13 +9,15 @@ int main()
     int arr[size_of_Arr];
 
     srand(time(0)); // Srand for generating random number
-    for (int i = 0; i < size_of_Arr; ++i) {
-        arr[i] = rand() % 100 ; // Random values between 0 to 1000
+    for (int i = 0; i < size_of_Arr; ++i) 
+    {
+        arr[i] = rand() % 100 ; // Random values between 0 to 100
+        // I only chose 100 since I only want low value numbers but 100 elements
     }
-  
     // Display the unsorted array which was declared that to be unsorted
     cout << "This is the unsorted Array:\n";
-    for (int i = 0; i < size_of_Arr; ++i) {
+    for (int i = 0; i < size_of_Arr; ++i) 
+    {
         cout << arr[i] << " ";
     }
     cout << endl;

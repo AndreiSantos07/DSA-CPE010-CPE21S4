dc04bacb330dfcf8c0fd474697e6ba2d903196b0
Andrei Santos <qarsantos02@tip.edu.ph>
2024-10-21 15:01:11 +0800

    Update MergeSort.h

--- Hands-on Activity 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Table 8-3. Merge Sort Algorithm/MergeSort.h
+++ Hands-on Activity 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Table 8-3. Merge Sort Algorithm/MergeSort.h
@@ -10,23 +10,22 @@ void merge(int arr[], int lft, int mid, int rght)
     int* L = new int[n1]; 
     int* R = new int[n2]; 
 
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[lft + i];
+    for (int a = 0; a < n1; a++)
+        L[a] = arr[lft + a];
     for (int j = 0; j < n2; j++)
         R[j] = arr[mid + 1 + j];
-
-    int i = 0; 
+        
+    int a = 0; 
     int j = 0; 
     int k = lft;
     
-    while (i < n1 && j < n2) 
+    while (a < n1 && j < n2) 
     {
         
-        if (L[i] <= R[j]) 
+        if (L[a] <= R[j]) 
         {
-            arr[k] = L[i];
-            i++;
+            arr[k] = L[a];
+            a++;
         } 
         
         else 
@@ -36,14 +35,14 @@ void merge(int arr[], int lft, int mid, int rght)
         }
         k++;
     }
-
-    while (i < n1) 
+    
+    while (a < n1) 
     {
-        arr[k] = L[i];
-        i++;
+        arr[k] = L[a];
+        a++;
         k++;
     }
-  
+    
     while (j < n2) 
     {
         arr[k] = R[j];
